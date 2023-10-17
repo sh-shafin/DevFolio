2d1161d759b8f95deb84dc0c12b2ee5add22425e
sadmanhosenshafin <sadmanhosenshafin@gmail.com>
2023-10-18 01:26:57 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// menu icon js
+let mobile_icon = document.getElementById("mobile_icon");
+let main_menu = document.getElementById("main_menu");
+
+mobile_icon.addEventListener("click", () => {
+  main_menu.classList.toggle("hidden");
+});
+
+// header bg add js
+const header = document.getElementById("header");
+window.onscroll = function () {
+  if (window.scrollY > 10) {
+    header.style.backgroundColor = "#191919";
+    header.style.padding = "10px 0";
+  } else {
+    header.style.backgroundColor = "transparent";
+    header.style.padding = "15px 0";
+  }
+};
+
+// auto type js
+var type = new Typed(".auto-type", {
+  strings: ["Designer", "Developer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// Swiper JS
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
