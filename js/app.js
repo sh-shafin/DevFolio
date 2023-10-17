1948d817903cef190d7159170f45013929d2b9d1
sadmanhosenshafin <sadmanhosenshafin@gmail.com>
2023-10-18 01:54:34 +0600

    change text size

--- js/app.js
+++ js/app.js
@@ -25,22 +25,3 @@ var type = new Typed(".auto-type", {
   backSpeed: 100,
   loop: true,
 });
-
-// Swiper JS
-// Initialize Swiper
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
